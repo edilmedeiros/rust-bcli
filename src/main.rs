6a897034b26fbe40b1c2e968ff166e69f61658a6
ion- <itornaza@gmail.com>
2024-03-06 22:04:52 +0200

    feat: Dumped options and option messages in the constants and included the constants module in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod constants;
+
 fn main() {
     println!("Hello, world!");
+
+    // Checked how to call an option message from the constants module
+    println!("{}", constants::GENERATE_MSG);
 }
