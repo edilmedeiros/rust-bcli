ba1831d871080286f349aaa20af01b1a04b6e7b5
ion- <itornaza@gmail.com>
2024-03-09 19:47:05 +0200

    feat: Implemented a basic structure for the project. It is intended to be a starting point and expected to adapt to requirements

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,12 +1,15 @@
 //
 // rpc.rs
 //
+// TODO: Include functions that get the user name and password from the 
+// bitcoin.conf file
+//
+#![allow(unused_imports)] // TODO: Remove when done
 
 use bitcoincore_rpc::{bitcoin, Auth, Client, Error, RpcApi};
 
 // Creates an rpc client used as a base to communicate with the API methods
-fn rpc(url: &str, user: &str, passwd: &str) -> Result<(), Error> {
-    let url = url.to_string()
+pub fn rpc(url: &str, user: &str, passwd: &str) -> Result<Client, Error> {
     let auth = Auth::UserPass(user.to_string(), passwd.to_string());
     Client::new(url, auth)
 }
