46c446c9bf8ea99e5d820f7ebc6ed6964b805ec9
Diogo Canut <diogocanut@hotmail.com>
2024-03-13 22:15:36 -0300

    Add Uptime command

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,44 @@
 #![allow(unused_variables)] // TODO: Remove when done
 
 // Project crates, only need to be imported in main
-mod display;
 mod commands;
 mod constants;
+mod display;
 mod parse;
 mod rpc;
+mod utils;
 
 // Project shortcuts
-use parse::Args;
-use parse::get_args;
 use commands::*;
+use parse::get_args;
+use parse::Args;
+use parse::Commands;
 
 // External crates shortcuts
 use bitcoincore_rpc::{bitcoin, Auth, Client, Error, RpcApi};
 
-fn main() {
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get the arguments from our wrapper parser
     let args = get_args();
 
     // Get the rpc client from our rpc wrapper module
-    let rpc = rpc::rpc(&args.url, &args.user, &args.pass).unwrap();
+    let rpc = rpc::rpc(&args.url, &args.user, &args.pass)?;
 
-    // Call our commands module! 
-    let best_block_hash = get_best_block_hash(&rpc);
+    match args.command {
+        Commands::GetBestBlockHash => {
+            let best_block_hash = get_best_block_hash(&rpc)?;
+            println!("Best block hash: {}", best_block_hash);
+        }
+        // Example for handling a hypothetical Uptime command
+        Commands::Uptime => {
+            let uptime = get_uptime(&rpc)?;
+        }
+    }
 
     /***
     Left here as reference from the example at:
     https://github.com/rust-bitcoin/rust-bitcoincore-rpc/blob/1b51e3d0bb614d36d256947f55d228ac0e1dc58f/client/examples/test_against_node.rs
-    
+
     let _blockchain_info = rpc.get_blockchain_info();
     let best_block_hash = rpc.get_best_block_hash().unwrap();
     println!("best block hash: {}", best_block_hash);
@@ -48,4 +57,5 @@ fn main() {
     println!("tx by `get`: {}", bitcoin_tx.txid());
     */
 
+    Ok(())
 }
