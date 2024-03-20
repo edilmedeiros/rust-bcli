87e844b5a74ed2ac134010ae41597b8b530b7361
Edil Medeiros <jose.edil@gmail.com>
2024-03-20 18:00:14 -0300

    Remove comment with old code

--- src/main.rs
+++ src/main.rs
@@ -40,29 +40,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    /***
-    Left here as reference from the example at:
-    https://github.com/rust-bitcoin/rust-bitcoincore-rpc/blob/1b51e3d0bb614d36d256947f55d228ac0e1dc58f/client/examples/test_against_node.rs
-
-    // Get the rpc client from our rpc wrapper module
-    let rpc = rpc::rpc(&args.url, &args.user, &args.pass).unwrap();
-
-    // Call our commands module! 
-    let best_block_hash = get_best_block_hash(&rpc);
-
-    let _blockchain_info = rpc.get_blockchain_info();
-    let best_block_hash = rpc.get_best_block_hash().unwrap();
-    println!("best block hash: {}", best_block_hash);
-    let bestblockcount = rpc.get_block_count().unwrap();
-    println!("best block height: {}", bestblockcount);
-    let best_block_hash_by_height = rpc.get_block_hash(bestblockcount).unwrap();
-    println!("best block hash by height: {}", best_block_hash_by_height);
-    assert_eq!(best_block_hash_by_height, best_block_hash);
-    let bitcoin_block: bitcoin::Block = rpc.get_by_id(&best_block_hash).unwrap();
-    println!("best block hash by `get`: {}", bitcoin_block.header.prev_blockhash);
-    let bitcoin_tx: bitcoin::Transaction = rpc.get_by_id(&bitcoin_block.txdata[0].txid()).unwrap();
-    println!("tx by `get`: {}", bitcoin_tx.txid());
-    */
-
     Ok(())
 }
