e94c9e4b88620b58854306b2cc7dc527556efab6
Edil Medeiros <jose.edil@gmail.com>
2024-03-25 17:39:56 -0300

    Add docstrings to parser structures
    These docstrings are used by clap to autogenerate help strings.
    Because of this, the next commit will remove the duplicate constants.

--- src/parse.rs
+++ src/parse.rs
@@ -16,19 +16,24 @@ use clap::{Parser, Subcommand};
 #[command(version, about, long_about = None)]
 pub struct Opts {
 
-    #[arg(long, default_value = "")]
-    pub conf: String,
+    /// Specify configuration file. Relative paths will be prefixed by datadir location.
+    //#[arg(long, default_value = "bitcoin.conf")]
+    //pub conf: String,
 
-    #[arg(long, default_value = "localhost")]
+    /// Send commands to the node running on <ip>
+    #[arg(long, value_name = "ip", default_value = "127.0.0.1")]
     pub rpcurl: String,
 
-    #[arg(long, default_value = "8332")]
+    /// Connect to JSON-RPC on <port>
+    #[arg(long, value_name = "port", default_value = "8332")]
     pub rpcport: String,
 
-    #[arg(long, default_value = "")]
+    /// Username for JSON_RPC connections
+    #[arg(long, value_name = "user")]
     pub rpcuser: String,
 
-    #[arg(long, default_value = "")]
+    /// Password for JSON-RPC connections
+    #[arg(long, value_name = "pw")]
     pub rpcpassword: String,
 
     #[command(subcommand)]
@@ -42,10 +47,13 @@ pub enum Command {
 
     // stop
 
-    #[command(name = GET_BEST_BLOCK_HASH, about = GET_BEST_BLOCK_HASH_MSG)]
+    /// Returns the hash of the best (tip) in the most-work fully-validated
+    /// chain.
+    #[command(name = "getbestblockhash")]
     GetBestBlockHash,
 
-    #[command(name = UPTIME, about = UPTIME)]
+    /// Returns the total uptime of the server.
+    #[command(name = "uptime")]
     Uptime,
 
 }
